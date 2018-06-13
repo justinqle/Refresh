cfc56bfc9760afcae6791c6415c34c5a3e72ef28
Justin Le <justinqle@gmail.com>
2018-06-13 05:07:46 -0400

    Added missing model classes from JSON Reddit

---
+++ app/src/main/java/com/example/android/waves/models/Listing.java
@@ -0,0 +1,31 @@
+package com.example.android.waves.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Listing {
+
+    @SerializedName("kind")
+    @Expose
+    private String kind;
+    @SerializedName("data")
+    @Expose
+    private Data data;
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+}
