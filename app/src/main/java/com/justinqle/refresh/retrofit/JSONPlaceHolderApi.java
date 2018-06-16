dcc917e23b6f6b2cc6e86891797e67c1f12d12e9
Justin Le <justinqle@gmail.com>
2018-06-16 01:15:39 -0400

    Added a GET request to retrieve listing after using the given query key from the current post

--- app/src/main/java/com/justinqle/refresh/retrofit/JSONPlaceHolderApi.java
+++ app/src/main/java/com/justinqle/refresh/retrofit/JSONPlaceHolderApi.java
@@ -4,10 +4,16 @@ import com.justinqle.refresh.models.Listing;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface JSONPlaceHolderApi {
 
     @GET("/.json")
     Call<Listing> getListing();
+
+    @GET("/.json")
+    Call<Listing> getListingAfter(
+            @Query("after") String nextKey
+    );
 
 }
