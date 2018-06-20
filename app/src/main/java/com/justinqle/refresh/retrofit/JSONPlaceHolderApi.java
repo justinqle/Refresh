77c3097da6d984235afa22eef547cc048de337e8
Justin Le <justinqle@gmail.com>
2018-06-19 23:49:40 -0400

    Added some Headers for Retrofit API (OAuth)

--- app/src/main/java/com/justinqle/refresh/retrofit/JSONPlaceHolderApi.java
+++ app/src/main/java/com/justinqle/refresh/retrofit/JSONPlaceHolderApi.java
@@ -5,17 +5,20 @@ import com.justinqle.refresh.models.Listing;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface JSONPlaceHolderApi {
 
     @GET("/best")
+    @Headers({"Content-Type: application/x-www-form-urlencoded", "User-Agent: Sample App"})
     Call<Listing> getListing(
             @Header("Authorization") String token,
             @Query("limit") int limit
     );
 
     @GET("/best")
+    @Headers({"Content-Type: application/x-www-form-urlencoded", "User-Agent: Sample App"})
     Call<Listing> getListingAfter(
             @Header("Authorization") String token,
             @Query("after") String nextKey,
