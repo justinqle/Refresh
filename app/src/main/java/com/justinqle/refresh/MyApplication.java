4a7a3a6d812404aaa59f83d84d406573ac63ce36
Justin Le <justinqle@gmail.com>
2018-08-16 13:50:52 -0400

    Updated Application class with JRAW components

--- app/src/main/java/com/justinqle/refresh/MyApplication.java
+++ app/src/main/java/com/justinqle/refresh/MyApplication.java
@@ -1,19 +1,68 @@
 package com.justinqle.refresh;
 
 import android.app.Application;
+import android.util.Log;
 
-public class MyApplication extends Application {
+import net.dean.jraw.android.AndroidHelper;
+import net.dean.jraw.android.AppInfoProvider;
+import net.dean.jraw.android.ManifestAppInfoProvider;
+import net.dean.jraw.android.SharedPreferencesTokenStore;
+import net.dean.jraw.android.SimpleAndroidLogAdapter;
+import net.dean.jraw.http.LogAdapter;
+import net.dean.jraw.http.SimpleHttpLogger;
+import net.dean.jraw.oauth.AccountHelper;
 
-    private static MyApplication mContext;
+import java.util.UUID;
 
-    public static MyApplication getContext() {
-        return mContext;
+public final class MyApplication extends Application {
+
+    private static AccountHelper accountHelper;
+    private static SharedPreferencesTokenStore tokenStore;
+
+    public static AccountHelper getAccountHelper() {
+        return accountHelper;
+    }
+
+    public static SharedPreferencesTokenStore getTokenStore() {
+        return tokenStore;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mContext = this;
-    }
 
+        // Get UserAgent and OAuth2 data from AndroidManifest.xml
+        AppInfoProvider provider = new ManifestAppInfoProvider(getApplicationContext());
+
+        // Ideally, this should be unique to every device
+        UUID deviceUuid = UUID.randomUUID();
+
+        // Store our access tokens and refresh tokens in shared preferences
+        tokenStore = new SharedPreferencesTokenStore(getApplicationContext());
+        // Load stored tokens into memory
+        tokenStore.load();
+        // Automatically save new tokens as they arrive
+        tokenStore.setAutoPersist(true);
+
+        // An AccountHelper manages switching between accounts and into/out of userless mode.
+        accountHelper = AndroidHelper.accountHelper(provider, deviceUuid, tokenStore);
+
+        // Every time we use the AccountHelper to switch between accounts (from one account to
+        // another, or into/out of userless mode), call this function
+        accountHelper.onSwitch(redditClient -> {
+            // By default, JRAW logs HTTP activity to System.out. We're going to use Log.i()
+            // instead.
+            LogAdapter logAdapter = new SimpleAndroidLogAdapter(Log.INFO);
+
+            // We're going to use the LogAdapter to write down the summaries produced by
+            // SimpleHttpLogger
+            redditClient.setLogger(
+                    new SimpleHttpLogger(SimpleHttpLogger.DEFAULT_LINE_LENGTH, logAdapter));
+
+            // If you want to disable logging, use a NoopHttpLogger instead:
+            // redditClient.setLogger(new NoopHttpLogger());
+
+            return null;
+        });
+    }
 }
