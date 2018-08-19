9d3312ce3a66e943051a76a94c43307cdedd545a
Justin Le <justinqle@gmail.com>
2018-08-19 00:01:52 -0400

    Added TODO

--- app/src/main/java/com/justinqle/refresh/architecture/SubmissionsDataSource.java
+++ app/src/main/java/com/justinqle/refresh/architecture/SubmissionsDataSource.java
@@ -22,6 +22,7 @@ public class SubmissionsDataSource extends PageKeyedDataSource<DefaultPaginator<
         // Builds paginator with requested load size specified by PagedList.Config
         DefaultPaginator<Submission> paginator = paginatorBuilder.limit(params.requestedLoadSize).build();
         // Grabs first page Listing
+        // TODO: try/catch to handle paginator.next() exceptions e.g. auth error (401)
         Listing<Submission> firstPage = paginator.next();
         // Passes the paginator to loadAfter()
         callback.onResult(firstPage, null, paginator);
