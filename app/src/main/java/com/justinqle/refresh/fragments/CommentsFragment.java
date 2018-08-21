3fd967267a86962d2151cc1a683fa82794ea3700
Justin Le <justinqle@gmail.com>
2018-08-21 15:25:21 -0400

    Comments Fragment has embedded toolbar

--- app/src/main/java/com/justinqle/refresh/fragments/CommentsFragment.java
+++ app/src/main/java/com/justinqle/refresh/fragments/CommentsFragment.java
@@ -1,19 +1,30 @@
 package com.justinqle.refresh.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.justinqle.refresh.R;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 public class CommentsFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comments, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_comments, container, false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(toolbar, navController);
+
+        return view;
     }
 
 }
