8f1f4222fa2a6e9f36732cc1423b1fb959e9684e
Justin Le <justinqle@gmail.com>
2018-08-20 21:30:05 -0400

    Implemented creation of bottom modal sheet for navigation and got rid of / modified drawer and old navigation view components

--- app/src/main/java/com/justinqle/refresh/activities/MainActivity.java
+++ app/src/main/java/com/justinqle/refresh/activities/MainActivity.java
@@ -3,20 +3,16 @@ package com.justinqle.refresh.activities;
 import android.os.Bundle;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageView;
 
 import com.justinqle.refresh.R;
+import com.justinqle.refresh.fragments.BottomNavigationDrawerFragment;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,58 +23,45 @@ public class MainActivity extends AppCompatActivity {
 
         // BottomAppBar
         BottomAppBar bar = findViewById(R.id.bar);
+        // Fragments can create custom options menu with setHasOptionsMenu()
         setSupportActionBar(bar);
+        // BottomNavigationDrawerFragment
+        BottomNavigationDrawerFragment bottomNavigationDrawerFragment = new BottomNavigationDrawerFragment();
+        // Clicking on BottomAppBar Navigation Icon shows BottomNavigationDrawerFragment
+        bar.setNavigationOnClickListener(view -> bottomNavigationDrawerFragment.show(getSupportFragmentManager(), bottomNavigationDrawerFragment.getTag()));
 
         // Floating Action Button
         FloatingActionButton fab = findViewById(R.id.fab);
+        // TODO: Snackbars cover bottom app bar
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show());
-
-        // Drawer (Container)
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, bar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        // Widget that can be used inside Drawer
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Listener to swap between Nav Menu and Account Switcher
-        View navHeader = navigationView.getHeaderView(0);
-        navHeader.setOnClickListener(view -> {
-            ImageView arrow = navHeader.findViewById(R.id.header_icon);
-            if (!view.isActivated()) {
-                view.setActivated(true);
-                arrow.animate().rotation(180).setDuration(200).start();
-                navigationView.getMenu().setGroupVisible(R.id.navigation, false);
-                navigationView.getMenu().setGroupVisible(R.id.account_switcher, true);
-            } else {
-                view.setActivated(false);
-                arrow.animate().rotation(0).setDuration(200).start();
-                navigationView.getMenu().setGroupVisible(R.id.account_switcher, false);
-                navigationView.getMenu().setGroupVisible(R.id.navigation, true);
-            }
-        });
 
         // Navigation Component
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        // Listener for every navigation change
+        // Listener for every navigation change to hide/show/modify overarching UI elements
         navController.addOnNavigatedListener((controller, destination) -> {
-            // Hides floating action button on fragments that don't utilize it
-            if (destination.getId() != R.id.postsFragment) {
+            // Closes BottomNavigationDrawer when Navigating
+            // TODO: There isn't a built-in way to close it on menu selection???
+            if (bottomNavigationDrawerFragment.isVisible()) {
+                bottomNavigationDrawerFragment.dismiss();
+            }
+            // Hides bottom app bar & floating action button on fragments that don't utilize it
+            if (destination.getId() == R.id.loginFragment) {
+                getSupportActionBar().hide();
                 fab.hide();
             }
+            // Makes sure bottom app bar gets shown again if hidden before
+            else if (!getSupportActionBar().isShowing()) {
+                getSupportActionBar().show();
+            }
         });
-        // Sets up Toolbar with Navigation Component
-        NavigationUI.setupActionBarWithNavController(this, navController, drawer);
-        NavigationUI.setupWithNavController(bar, navController, drawer);
-        // Sets up Navigation View with Navigation Component
-        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
+    // Closes sidebar on back press
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
         } else {
             super.onBackPressed();
         }
@@ -88,4 +71,5 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
     }
+
 }
